a7b13e23ead618b63148a498bb349e7f6549ef4c
oddster08 <ayushguptaa20@gmail.com>
2024-05-03 16:05:09 +0530

    client updated

---
+++ src/store/Slices/videoSlice.js
@@ -0,0 +1,202 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axiosInstance from "../../helpers/axiosInstance";
+import toast from "react-hot-toast";
+import { BASE_URL } from "../../constants";
+
+const initialState = {
+    loading: false,
+    uploading: false,
+    uploaded: false,
+    videos: {
+        docs: [],
+        hasNextPage: false,
+    },
+    ownVideos:{
+        docs: [],
+        hasNextPage: false,
+    },
+    video: null,
+    publishToggled: false,
+};
+
+export const getAllVideos = createAsyncThunk(
+    "getAllVideos",
+    async ({ userId, sortBy, sortType, query, page, limit }) => {
+        try {
+            
+            const url = new URL(`${BASE_URL}/video`);
+           
+            if (userId) url.searchParams.set("userId", userId);
+            if (query) url.searchParams.set("query", query);
+            if (page) url.searchParams.set("page", page);
+            if (limit) url.searchParams.set("limit", limit);
+            if (sortBy && sortType) {
+                url.searchParams.set("sortBy", sortBy);
+                url.searchParams.set("sortType", sortType);
+            }
+          //    console.log(url)
+            const response = await axiosInstance.get("/videos");
+            let filteredResp = response.data.data.docs
+            .map((i) => {
+                if (i.owner === userId) return i;
+                else return null; // Return null for elements that don't match the condition
+            })
+            .filter((i) => i !== null);
+            console.log(filteredResp)
+            //console.log(response.data.data.docs[0])
+
+            return response.data.data;
+        } catch (error) {
+            toast.error(error?.response?.data?.error);
+            throw error;
+        }
+    }
+);
+
+export const publishAvideo = createAsyncThunk("publishAvideo", async (data) => {
+    const formData = new FormData();
+    formData.append("title", data.title);
+    formData.append("description", data.description);
+    formData.append("videoFile", data.videoFile[0]);
+    formData.append("thumbnail", data.thumbnail[0]);
+
+    try {
+        const response = await axiosInstance.post("/videos/publish", formData, {
+            withCredentials: true
+          });
+          
+        toast.success(response?.data?.message);
+        return response.data.data;
+    } catch (error) {
+        toast.error(error?.response?.data?.error);
+        throw error;
+    }
+});
+
+export const updateAVideo = createAsyncThunk(
+    "updateAVideo",
+    async ({ videoId, data }) => {
+        const formData = new FormData();
+        formData.append("title", data.title);
+        formData.append("description", data.description);
+        formData.append("thumbnail", data.thumbnail[0]);
+
+        try {
+            const response = await axiosInstance.patch(
+                `/videos/${videoId}`,
+                formData
+            );
+            toast.success(response?.data?.message);
+            return response.data.data;
+        } catch (error) {
+            toast.error(error?.response?.data?.error);
+            throw error;
+        }
+    }
+);
+
+export const deleteAVideo = createAsyncThunk(
+    "deleteAVideo",
+    async (videoId) => {
+        try {
+            const response = await axiosInstance.delete(`/videos/${videoId}`);
+            toast.success(response?.data?.message);
+            return response.data.data;
+        } catch (error) {
+            toast.error(error?.response?.data?.error);
+            throw error;
+        }
+    }
+);
+
+export const getVideoById = createAsyncThunk(
+    "getVideoById",
+    async ({ videoId }) => {
+        console.log(videoId)
+        try {
+         const response = await axiosInstance.get(`/videos/${videoId}`, {
+          withCredentials: true
+           
+     }); 
+           console.log(response.data.data)
+            return response.data.data;
+        } catch (error) {
+            toast.error(error?.response?.data?.error);
+            throw error;
+        }
+    }
+);
+
+export const togglePublishStatus = createAsyncThunk(
+    "togglePublishStatus",
+    async (videoId) => {
+        try {
+            const response = await axiosInstance.patch(
+                `/videos/toggle/publish/${videoId}`
+            );
+            toast.success(response.data.message);
+            return response.data.data.isPublished;
+        } catch (error) {
+            toast.error(error?.response?.data?.error);
+            throw error;
+        }
+    }
+);
+
+const videoSlice = createSlice({
+    name: "video",
+    initialState,
+    reducers: {
+        updateUploadState: (state) => {
+            state.uploading = false;
+            state.uploaded = false;
+        },
+        makeVideosNull: (state) => {
+            state.videos.docs = [];
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(getAllVideos.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(getAllVideos.fulfilled, (state, action) => {
+            state.loading = false;
+            state.videos.docs = [...state.videos.docs, ...action.payload.docs];
+            state.videos.hasNextPage = action.payload.hasNextPage;
+        });
+        builder.addCase(publishAvideo.pending, (state) => {
+            state.uploading = true;
+        });
+        builder.addCase(publishAvideo.fulfilled, (state) => {
+            state.uploading = false;
+            state.uploaded = true;
+        });
+        builder.addCase(updateAVideo.pending, (state) => {
+            state.uploading = true;
+        });
+        builder.addCase(updateAVideo.fulfilled, (state) => {
+            state.uploading = false;
+            state.uploaded = true;
+        });
+        builder.addCase(deleteAVideo.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(deleteAVideo.fulfilled, (state) => {
+            state.loading = false;
+        });
+        builder.addCase(getVideoById.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(getVideoById.fulfilled, (state, action) => {
+            state.loading = false;
+            state.video = action.payload;
+        });
+        builder.addCase(togglePublishStatus.fulfilled, (state) => {
+            state.publishToggled = !state.publishToggled;
+        });
+    },
+});
+
+export const { updateUploadState, makeVideosNull } = videoSlice.actions;
+
+export default videoSlice.reducer;
